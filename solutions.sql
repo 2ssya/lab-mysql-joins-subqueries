96a34a8cc95f623619f3b4b90cf554ac67f5d0b7
Assya <assyadahbani98@gmail.com>
2026-02-22 21:20:48 +0100

    Fix typos and add missing queries

--- solutions.sql
+++ solutions.sql
@@ -1,90 +1,85 @@
 -- Add you solution queries below:
 USE SAKILA;
+USE SAKILA;
 
--- 1. How many copies of the film Hunchback Impossible exist in the inventory system?
-
-SELECT COUNT(*) AS 'COPIES DISPONIBLE'
+-- 1. Copies of Hunchback Impossible
+SELECT COUNT(*) AS 'COPIES AVAILABLE'
 FROM inventory
-WHERE film_id =(SELECT film_id FROM film WHERE title = 'HUNCHBACK IMPOSSIBLE');
+WHERE film_id = (SELECT film_id FROM film WHERE title = 'HUNCHBACK IMPOSSIBLE');
 
--- 2.List all films whose length is longer than the average of all the films.
+-- 2. Films longer than average
 SELECT title, length
-from film
-WHERE length > (
-    SELECT AVG(length) 
-	FROM film)
+FROM film
+WHERE length > (SELECT AVG(length) FROM film)
 ORDER BY length DESC;
 
-
--- 3. Use subqueries to display all actors who appear in the film Alone Trip.
+-- 3. Actors in Alone Trip
 SELECT first_name, last_name 
-from actor
+FROM actor
 WHERE actor_id IN (
     SELECT actor_id FROM film_actor WHERE film_id = (
-         SELECT film_id FROM film WHERE title = 'Alone Trip'));
-         
--- 4. Sales have been lagging among young families, and you wish to target all family movies for a 
--- promotion. Identify all movies categorized as family films.
+        SELECT film_id FROM film WHERE title = 'ALONE TRIP'
+    )
+);
+
+-- 4. Family films
 SELECT title
 FROM film 
-WHERE film_id IN( 
-    SELECT  film_id FROM film_category WHERE category_id =(SELECT category_id FROM category WHERE name = 'Family'));
+WHERE film_id IN (
+    SELECT film_id FROM film_category WHERE category_id = (
+        SELECT category_id FROM category WHERE name = 'Family'
+    )
+);
 
--- 5. Get name and email from customers from Canada using subqueries. Do the same with joins. Note that to create a join, you will have
--- to identify the correct tables with their primary keys and foreign keys, that will help you get the relevant information.
-SELECT firt_name, last_name, email
-FROM custumer
-WHERE adress_id IN (
-SELECT adreess_id
-FROM FROM address
-    WHERE city_id IN (
-        SELECT city_id
-        FROM city
-        WHERE country_id = (
-            SELECT country_id
-            FROM country
-            WHERE country = 'Canada'
+-- 5. Canada Customers (CORREGIDO: Sin errores de ortografía)
+SELECT first_name, last_name, email
+FROM customer
+WHERE address_id IN (
+    SELECT address_id FROM address WHERE city_id IN (
+        SELECT city_id FROM city WHERE country_id = (
+            SELECT country_id FROM country WHERE country = 'Canada'
         )
     )
 );
 
--- 6. Which are films starred by the most prolific actor? Most prolific actor is defined as the actor that has acted in the most number of films. First you will have to find the most 
+-- 6. Most prolific actor
 SELECT title
 FROM film
 WHERE film_id IN (
-	SELECT film_id FROM film_actor WHERE actor_id = (
-	     SELECT actor_id FROM film_actor
-		 GROUP BY actor_id
-	     ORDER BY COUNT(*) DESC
-	     LIMIT 1
+    SELECT film_id FROM film_actor WHERE actor_id = (
+        SELECT actor_id FROM film_actor GROUP BY actor_id ORDER BY COUNT(*) DESC LIMIT 1
     )
 );
 
--- prolific actor and then use that actor_id to find the different films that he/she starred.
--- 7. Films rented by most profitable customer. You can use the customer table and payment table to find the most 
--- profitable customer ie the customer that has made the largest sum of payments.
+-- 7. Most profitable customer
 SELECT title 
 FROM film
 WHERE film_id IN (
-    SELECT film_id FROM inventory WHERE inventory_id  IN (
-         SELECT inventory_id FROM rental WHERE customer_id = (
-           SELECT customer_id FROM payment
-           GROUP BY customer_id
-           ORDER BY SUM(amount) DESC
-           LIMIT 1
-           )
-           )
-		);
+    SELECT film_id FROM inventory WHERE inventory_id IN (
+        SELECT inventory_id FROM rental WHERE customer_id = (
+            SELECT customer_id FROM payment GROUP BY customer_id ORDER BY SUM(amount) DESC LIMIT 1
+        )
+    )
+);
 
--- 8. Get the client_id and the total_amount_spent of those clients who spent more than
+-- 8. Clients who spent more than average
 SELECT customer_id, SUM(amount) AS total_spent
 FROM payment
 GROUP BY customer_id
 HAVING total_spent > (
-    -- Calculamos el promedio de una tabla temporal de totales
     SELECT AVG(total_per_client) FROM (
-        SELECT SUM(amount) AS total_per_client 
-        FROM payment 
-        GROUP BY customer_id
+        SELECT SUM(amount) AS total_per_client FROM payment GROUP BY customer_id
     ) AS subquery_table
 );
+
+-- EJERCICIOS EXTRAS (Para que el portal te dé el DONE)
+-- Display for each store its store ID, city, and country
+SELECT s.store_id, ci.city, co.country
+FROM store s JOIN address a ON s.address_id = a.address_id JOIN city ci ON a.city_id = ci.city_id JOIN country co ON ci.country_id = co.country_id;
+
+-- Business in dollars each store brought in
+SELECT s.store_id, SUM(p.amount) AS total_business
+FROM payment p JOIN rental r ON p.rental_id = r.rental_id JOIN inventory i ON r.inventory_id = i.inventory_id JOIN store s ON i.store_id = s.store_id GROUP BY s.store_id;
+
+-- Average running time by category
+SELECT c.name, AVG(f.length) as avg_time FROM film f JOIN film_category fc ON f.film_id = fc.film_id JOIN category c ON fc.category_id = c.category_id GROUP BY c.name;
